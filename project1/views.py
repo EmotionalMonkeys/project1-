933b70c126485d441ddad8d7cfcc6c93971d8bd8
annchih <hchih@ucsd.edu>
2016-04-26 00:26:04 -0700

    Can save form data

--- project1/views.py
+++ project1/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from .forms import CategoryForm
 def category(request):
    return render(request, 'project1/category.html',{})
+
 def category_new(request):
-   form = CategoryForm()
+   if request.method == "POST": #Back with form data
+      form = CategoryForm(request.POST) 
+      if form.is_valid():
+         category = form.save()
+         category.save()
+
+   else: #Access page 1st time => blank form
+      form = CategoryForm()
+
    return render(request, 'project1/category_edit.html',{'form':form})
