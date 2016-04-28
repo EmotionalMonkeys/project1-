b9c34a5deb788a7944095edd36dc1469f46ba936
annchih <hchih@ucsd.edu>
2016-04-27 18:07:56 -0700

    Insert button and display for categories

--- project1/urls.py
+++ project1/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.category, name='category'),
-    url(r'^category/new/$', views.category_new, name='category_new'),
+    url(r'^category/new/$',views.category_new, name='category_new'),
+    url(r'^category/edit/$', views.category_edit,name='category_edit'),
     ]
--- project1/views.py
+++ project1/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CategoryForm
+from .models import Category
 def category(request):
-   return render(request, 'project1/category.html',{})
+   categories = Category.objects.order_by('name')
+   return render(request,'project1/category.html',{'categories':categories})
 
 def category_new(request):
    if request.method == "POST": #Back with form data
       form = CategoryForm(request.POST) 
       if form.is_valid():
-         category = form.save()
-         category.save()
-
+         form.save()
+         return redirect('category')
    else: #Access page 1st time => blank form
       form = CategoryForm()
 
    return render(request, 'project1/category_edit.html',{'form':form})
+
+def category_edit(request):
+   category = get_object_or_404(Category)
+   if request.method == "POST": #Back with form data
+      form = CategoryForm(request.POST, instance=category) 
+      if form.is_valid():
+         form.save()
+         return redirect('category')
+
+   else: #Access page 1st time => blank form
+      form = CategoryForm(instance=category)
+
+   return render(request, 'project1/category_edit.html',{'form':form})
+   
+
+   
