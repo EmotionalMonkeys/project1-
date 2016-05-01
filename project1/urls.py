ec8258f5e1825328749e9723b812a972e71bf118
annchih <hchih@ucsd.edu>
2016-05-01 11:45:26 -0700

    working insert update

--- project1/urls.py
+++ project1/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
   	url(r'^$', views.homepage, name='homepage'),
-  	url(r'^product', views.product, name='product'),
-  	url(r'^category', views.category, name='category'),
+  	url(r'^product/$', views.product, name='product'),
+  	url(r'^category/$', views.category, name='category'),
     url(r'^category/new/$', views.category_new, name='category_new'),
     url(r'^category/edit/(?P<pk>\d+)/$', views.category_edit, name='category_edit'),
 ]
